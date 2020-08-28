1cfb2176a33448652872eba279fd1046641c9177
Valeraanaconda <43722062+Valeraanaconda@users.noreply.github.com>
2020-08-28 19:53:17 +0300

    Door

---
+++ Vironit_Project/Assets/Scripts/Door/OpenDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public Animator open_door;
+    void Start()
+    {
+        open_door = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            open_door.Play("Open_door");
+        }
+
+    }
+}
