b69f33f44c611719a6e6503376dea198b7cd9d61
MetraSib <70145795+MetraSib@users.noreply.github.com>
2020-09-01 15:31:34 +0300

    Added VideoOnTV
    +localization

---
+++ Vironit_Project/Assets/Scripts/Player/UseGameObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseGameObject : MonoBehaviour
+{
+    public GameObject monitor;
+    public GameObject textE;
+    private Camera playerCamera;
+
+    private void Start()
+    {
+        playerCamera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        GetObject();
+    }
+
+    public void GetObject() 
+    {
+        int layerMask = 00001000;
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(playerCamera.pixelWidth / 2, playerCamera.pixelHeight / 2, 0));
+
+        if (Physics.Raycast(ray, 1.5f, layerMask))
+        {
+            textE.SetActive(true);
+
+            //Debug.DrawRay(ray.origin, ray.direction*10, Color.yellow);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (monitor.activeSelf) monitor.SetActive(false);
+                else monitor.SetActive(true);
+            }
+        }
+        else
+        {
+            textE.SetActive(false);
+        }
+    }
+}
