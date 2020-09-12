07e9c3638b9cd11f8e6b689e814d4f2b219537ee
Valeraanaconda <43722062+Valeraanaconda@users.noreply.github.com>
2020-09-12 13:37:17 +0300

    safe

---
+++ Vironit_Project/Assets/Scripts/Safe/SafeCode/BushButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BushButton : MonoBehaviour
+{
+    public Text result_text;
+    
+    public void addequale()
+    {
+        result_text.text = "1";
+    }
+}
